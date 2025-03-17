e702526d10ff8cf13d7c337c369e482aa6fc304b
f-lab-devin <devin@f-lab.kr>
2025-03-17 20:32:55 +0900

    json generator 구현

--- src/generator.ts
+++ src/generator.ts
@@ -2,6 +2,7 @@ import generate from "@babel/generator";
 import * as t from "@babel/types";
 import * as fs from "fs";
 import * as prettier from "prettier";
+import { ExtractedText } from "./core/type";
 
 export class Generator {
   private enablePrettier: boolean;
@@ -14,7 +15,21 @@ export class Generator {
 
     const formattedCode = await this.formatCode(code);
 
-    this.writeCode(formattedCode, filePath);
+    this.writeFile(formattedCode, filePath);
+  }
+
+  async generateJson(
+    data: ExtractedText[],
+    locales: string[],
+    outputDir: string
+  ): Promise<void> {
+    const formattedData = this.formatExtractedText(data);
+    const json = JSON.stringify(formattedData, null, 2);
+
+    locales.forEach((locale) => {
+      const filePath = `${outputDir}/${locale}.json`;
+      this.writeFile(json, filePath);
+    });
   }
 
   private generateCode(ast: t.File): string {
@@ -23,8 +38,8 @@ export class Generator {
     }).code;
   }
 
-  private writeCode(code: string, filePath: string): void {
-    fs.writeFileSync(filePath, code);
+  private writeFile(content: string, filePath: string): void {
+    fs.writeFileSync(filePath, content);
   }
 
   private async formatCode(code: string): Promise<string> {
@@ -39,5 +54,54 @@ export class Generator {
       ...config,
       parser: "babel-ts",
     });
+  }
+
+  private formatExtractedText(data: ExtractedText[]): Record<string, string> {
+    // trwapper 분리
+
+    const twrappedTexts = data.filter((item) => item.isTWrapped);
+
+    // trwpper 아닌놈들은 containerName을 key로 그룹화
+    const notTwrappedTexts = data.filter((item) => !item.isTWrapped);
+    const groupedTexts = notTwrappedTexts.reduce<
+      Record<string, ExtractedText[]>
+    >((acc, item) => {
+      if (!acc[item.containerName]) {
+        acc[item.containerName] = [];
+      }
+      acc[item.containerName].push(item);
+      return acc;
+    }, {});
+
+    // record 형태로 만들기
+
+    return {
+      ...this.plainJson(twrappedTexts),
+      ...this.groupToPlainJson(groupedTexts),
+    };
+  }
+
+  private plainJson(data: ExtractedText[]): Record<string, string> {
+    const result: Record<string, string> = {};
+
+    data.forEach((item) => {
+      result[item.text] = "";
+    });
+
+    return result;
+  }
+
+  private groupToPlainJson(
+    data: Record<string, ExtractedText[]>
+  ): Record<string, string> {
+    const result: Record<string, string> = {};
+
+    Object.keys(data).forEach((key, index) => {
+      result[`__comment_${index}`] = key;
+      data[key].forEach((item) => {
+        result[item.text] = "";
+      });
+    });
+    return result;
   }
 }
