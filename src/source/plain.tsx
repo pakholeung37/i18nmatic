2db2ce62e5d1c2296929e054517433b0a1bf0cef
f-lab-devin <devin@f-lab.kr>
2025-03-18 10:54:22 +0900

    plainText 데모 추가

---
+++ src/source/plain.tsx
@@ -0,0 +1,15 @@
+const ERROR_MESSAGE = "잠시후 다시 시도해주세요";
+const review = {
+  id: 1,
+  name: "테스트",
+};
+const reviews = [
+  {
+    id: 1,
+    name: "김X님",
+  },
+  {
+    id: 2,
+    name: "양X님",
+  },
+];
