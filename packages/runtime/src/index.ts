fcc5e44632012b67ae1f1582e9d578be80c62b18
Pakho Leung <pakholeung37@gmail.com>
2025-07-23 09:50:07 +0800

    Bump version to 0.5.2 in CLI and runtime package.json files Fix window.Ti18n assignment in runtime index.ts

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -7,7 +7,7 @@ export type * from "i18next"
 export const i18n: I18n =
   ((window as any).Ti18n as typeof i18next) ?? i18next.createInstance()
 // @ts-ignore
-window.Ti18n = i18n // Expose for debugging
+window.Ti18n = window.Ti18n ?? i18n
 
 // 绑定 t 函数以便直接使用ng
 export const t: I18n["t"] = i18n.t.bind(i18n)
