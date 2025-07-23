32bdf817002f39dcff6117b345eb9ff27d08d345
Pakho Leung <pakholeung37@gmail.com>
2025-07-23 10:04:03 +0800

    Refactor t function to improve translation handling

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -7,7 +7,20 @@ export type * from "i18next"
 export const i18n: I18n =
   ((window as any).Ti18n as typeof i18next) ?? i18next.createInstance()
 // @ts-ignore
-window.Ti18n = window.Ti18n ?? i18n
+window.Ti18n = i18n
 
-// 绑定 t 函数以便直接使用ng
-export const t: I18n["t"] = i18n.t.bind(i18n)
+export const t: I18n["t"] = function t(...args) {
+  // 如果没有第三参数
+  if (!args[2]) {
+    return i18n.t(...args)
+  }
+  const defaultMessage = args[2]
+  const key = args[0]
+  let result = i18n.t(...args)
+  // 如果没有翻译结果，使用默认消息
+  if (result === key) {
+    // 如果没有翻译结果，使用默认消息
+    result = defaultMessage as any
+  }
+  return result
+} as I18n["t"]
